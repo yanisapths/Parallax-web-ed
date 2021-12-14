c70bd9988c5eb6ae334c81ecef868f8cdcd90535
yanisapths <72002605+yanisapths@users.noreply.github.com>
2021-12-14 20:35:55 +0700

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+    .to(".rock1", 3,{y: -300})
+    .to(".girl1", 3,{y: -200}, "-=3")
+    .fromTo(".bg1", 3, {y: -50},{y: 0, duration: 3}, "-=3")
+    .to('.content', 3,{top:'0%'}, "-=3")
+    .fromTo('.content-images',{opacity:0},{opacity:1,duraion: 3})
+    .fromTo('.text',{opacity:0},{opacity:1,duraion: 3});
+
+
+    let scene = new ScrollMagic.Scene({
+        triggerElement: "section", 
+        duration: "300%", 
+        triggerHook: 0,
+    })
+    .setTween(timeline)
+    .setPin("section")
+    .addTo(controller);
